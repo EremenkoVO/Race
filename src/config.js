b092654b8606a638dd9c4893fdd5dd7c5d53a228
Vadim Eremenko <eremenko.v.o@yandex.ru>
2020-10-27 18:49:59 +0300

    configuration project

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import Phaser from "phaser";
+
+var config = {
+  type: Phaser.AUTO,
+  wigth: 1280,
+  height: 720,
+  scene: [],
+};
+
+export default config;
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,27 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const merge = require("webpack-merge");
+const path = require("path");
+const base = require("./base");
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = merge(common, {
+module.exports = merge(base, {
   mode: "production",
+  output: {
+    filename: "bundle.min.js",
+  },
+  devtool: false,
+  performance: {
+    maxEntrypointSize: 900000,
+    maxAssetSize: 900000,
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
 });
