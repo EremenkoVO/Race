c0f77188ee281a7618e971d5eea684cb88c7c079
Vadim Eremenko <eremenko.v.o@yandex.ru>
2020-10-28 18:05:16 +0300

    tiles objects

--- src/scripts/scenes/PreloadScene.js
+++ src/scripts/scenes/PreloadScene.js
@@ -2,6 +2,8 @@ import Phaser from "phaser";
 import LoadingBar from "../classes/LoadingBar";
 import tilesetPng from "../../assets/sprites/tileset.png";
 import tilemapJson from "../../assets/sprites/tilemap.json";
+import objectsPng from "../../assets/sprites/objects.png";
+import objectsJson from "../../assets/sprites/objects.json";
 
 class PreloadScene extends Phaser.Scene {
   constructor() {
@@ -17,6 +19,7 @@ class PreloadScene extends Phaser.Scene {
       frameHeight: 64,
     });
     this.load.tilemapTiledJSON("tilemap", tilemapJson);
+    this.load.atlas("objects", objectsPng, objectsJson);
   }
 
   create() {
